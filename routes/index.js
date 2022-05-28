2d58a0ee5c618d65c24362e35ac6b85732127bc0
Josech03 <whitesox3734@gmail.com>
2022-05-28 18:44:47 -0400

    Task3

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@ var router = express.Router();
 const sqlite3=require('sqlite3').verbose();
 const http=require('http');
 const path = require('path');
+const geoip = require('geoip-lite');
+const nodemailer = require('nodemailer');
 
 
 const db=path.join(__dirname,"basededatos","sqlitedb.db");
@@ -14,9 +16,8 @@ if (err){
 }
 })
 
-const crear="CREATE TABLE IF NOT EXISTS contacts(email VARCHAR(16),nombre VARCHAR(16), comentario TEXT,fecha DATATIME,ip VARCHAR(15));";
-
-
+const crear="CREATE TABLE IF NOT EXISTS contacts(email VARCHAR(16),nombre VARCHAR(16), comentario TEXT,fecha DATATIME,ip VARCHAR(15), pais VARCHAR(20));";
+require('dotenv').config();
 
 db_run.run(crear,err=>{
 	if (err){
@@ -26,7 +27,9 @@ db_run.run(crear,err=>{
 }
 })
 router.get('/',(req,res)=>{
-	res.render('index.ejs',{ct:{}})
+	res.render('index.ejs',{ct:{},
+	CLAVE_RECAPTCHA:process.env.CLAVE_RECAPTCHA,
+  	GOOGLE_ANALYTICS:process.env.GOOGLE_ANALYTICS})	
 });
 
 router.get('/contactos',(req,res)=>{
@@ -54,8 +57,12 @@ router.post('/',(req,res)=>{
  	 } else {
 	ip = req.connection.remoteAddress;
   	}
-	const sql="INSERT INTO contacts(email, nombre, comentario, fecha,ip) VALUES (?,?,?,?,?)";
-	const nuevos_mensajes=[req.body.email, req.body.nombre, req.body.comentario,fech,ip];
+  	var geo = geoip.lookup(ip);
+	console.log(geo);
+	var pais = geo.country;
+	const sql="INSERT INTO contacts(email, nombre, comentario, fecha, ip, pais) VALUES (?,?,?,?,?,?)";
+	const nuevos_mensajes=[req.body.email, req.body.nombre, req.body.comentario,fech,ip,pais];
+	
 	db_run.run(sql, nuevos_mensajes, err =>{
 	if (err){
 		return console.error(err.message);
@@ -63,6 +70,42 @@ router.post('/',(req,res)=>{
 	else{
 		res.redirect("/");
 		}
+		let transporter = nodemailer.createTransport({
+					host: "smtp-mail.outlook.com",
+    				secureConnection: false, 
+    				port: 587, 
+    				auth: {
+       				 user: process.env.CORREO,
+       				 pass: process.env.CLAVE
+
+    				},
+    					tls: {
+      					ciphers:'SSLv3'
+   					}
+			});
+				const Message = `
+					<p>Programacion 2 Sec-3</p>
+					<h3>Información del usuario:</h3>
+					<ul>
+					<li>Email: ${req.body.email}</li>
+					<li>Nombre: ${req.body.nombre}</li>
+					<li>Comentario: ${req.body.comentario}</li>
+					<li>Fecha-Hora: ${fech}</li>
+					<li>IP: ${ip}</li>
+					<li>Pais: ${pais}</li>
+					</ul>`;
+				const receiverAndTransmitter = {
+					from: 'p2_30276873@outlook.es',
+					to: 'programacion2ais@dispostable.com, p2_30276873@dispostable.com',
+					subject: 'Informacion del contacto', 
+					html: Message
+				};
+				transporter.sendMail(receiverAndTransmitter,(err, info) => {
+					if(err)
+						console.log(err)
+					else
+						console.log(info);
+					})
 	})
 });
 
